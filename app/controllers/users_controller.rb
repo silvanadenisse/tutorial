172665c68537dfd450b83d9dcf87e61d3b8e75d5
silvana.denisse.15@gmail.com <silvana.denisse.15@gmail.com>
2017-03-27 00:16:54 -0700

    signup error users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+    debugger
   end
 
   # GET /users/new
@@ -30,10 +32,18 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
+    end
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
   end
 
